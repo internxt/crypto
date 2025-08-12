ce9283001593a495cab27873a21caa72dab9a3be
tamarafinogina <tamara@internxt.com>
2025-08-12 17:31:34 +0200

    fix typo

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,4 @@ export * as core from './core';
 export * as email from './email';
 export * as keys from './keys';
 export * as utils from './utils';
-export * as keystrore from './keystore';
+export * as keystore from './keystore';
