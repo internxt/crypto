8317d18aa8a0e1a72b61fb7bb9847e966e1dfb39
tamarafinogina <tamara@internxt.com>
2026-01-14 18:16:31 +0100

    load modules separately

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,106 @@
-export * as asymmetric from './asymmetric-crypto';
-export * as deriveKey from './derive-key';
-export * as emailCrypto from './email-crypto';
-export * as emailSearch from './email-search';
-export * as emailService from './email-service';
-export * as hash from './hash';
-export * as keyWrapper from './key-wrapper';
-export * as keystoreCrypto from './keystore-crypto';
-export * as keystoreService from './keystore-service';
-export * as pq from './post-quantum-crypto';
-export * as storage from './storage-service';
-export * as symmetric from './symmetric-crypto';
-export * as utils from './utils';
+export {
+  deriveSecretKey,
+  generateEccKeys,
+  exportPublicKey,
+  importPublicKey,
+  exportPrivateKey,
+  importPrivateKey,
+} from './asymmetric-crypto';
+export {
+  deriveSymmetricKeyFromTwoKeys,
+  deriveSymmetricCryptoKeyFromTwoKeys,
+  deriveSymmetricKeyFromTwoKeysAndContext,
+  deriveSymmetricKeyFromContext,
+  deriveSymmetricCryptoKeyFromContext,
+  getKeyFromPassword,
+  getKeyFromPasswordAndSalt,
+  getKeyFromPasswordHex,
+  getKeyFromPasswordAndSaltHex,
+  verifyKeyFromPasswordHex,
+} from './derive-key';
+export {
+  encryptEmailHybrid,
+  encryptEmailHybridForMultipleRecipients,
+  decryptEmailHybrid,
+  encryptEmailAndSubjectHybrid,
+  encryptEmailAndSubjectHybridForMultipleRecipients,
+  decryptEmailAndSubjectHybrid,
+  createPwdProtectedEmail,
+  decryptPwdProtectedEmail,
+  createPwdProtectedEmailAndSubject,
+  decryptPwdProtectedEmailAndSubject,
+  generateEmailKeys,
+  paramsToBase64,
+  base64ToParams,
+  hybridEncyptedEmailToBase64,
+  pwdProtectedEmailToBase64,
+} from './email-crypto';
+export {
+  openDatabase,
+  closeDatabase,
+  deriveIndexKey,
+  encryptAndStoreEmail,
+  encryptAndStoreManyEmail,
+  getAndDecryptEmail,
+  getAndDecryptAllEmails,
+  deleteEmail,
+  getEmailCount,
+  deleteOldestEmails,
+  enforceMaxEmailNumber,
+  getAllEmailsSortedNewestFirst,
+  getAllEmailsSortedOldestFirst,
+  getEmailBatch,
+  createCacheFromDB,
+  getEmailFromCache,
+  deleteEmailFromCache,
+  addEmailsToCache,
+  addEmailToCache,
+  addEmailToSearchIndex,
+  removeEmailFromSearchIndex,
+  buildSearchIndexFromCache,
+  searchEmails,
+} from './email-search';
+export { getEmailServiceAPI } from './email-service';
+export {
+  hashDataArray,
+  hashDataArrayWithKey,
+  hashDataArrayHex,
+  hashDataArrayWithKeyHex,
+  getBytesFromData,
+  getBytesFromDataHex,
+  getBytesFromDataArrayHex,
+  computeMac,
+} from './hash';
+export { unwrapKey, wrapKey } from './key-wrapper';
+export { createEncryptionAndRecoveryKeystores, openEncryptionKeystore, openRecoveryKeystore } from './keystore-crypto';
+export { getKeyServiceAPI } from './keystore-service';
+export { generateKyberKeys, encapsulateKyber, decapsulateKyber } from './post-quantum-crypto/kyber768';
+export {
+  encryptSymmetrically,
+  decryptSymmetrically,
+  importSymmetricCryptoKey,
+  exportSymmetricCryptoKey,
+  genSymmetricCryptoKey,
+  genSymmetricKey,
+  deriveSymmetricCryptoKey,
+} from './symmetric-crypto';
+export {
+  uint8ArrayToHex,
+  UTF8ToUint8,
+  uint8ToUTF8,
+  hexToUint8Array,
+  uint8ArrayToBase64,
+  base64ToUint8Array,
+  genMnemonic,
+  encryptedKeystoreToBase64,
+  base64ToEncryptedKeystore,
+  base64ToPublicKey,
+  publicKeyToBase64,
+  generateUuid,
+  uuidToBytes,
+  bytesToUuid,
+  mnemonicToBytes,
+  bytesToMnemonic,
+} from './utils';
 export * from './types';
 export * from './constants';
